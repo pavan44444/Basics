57d8307e6e0a2276d0762427d03584316720405e
pavan <128489562+pavan44444@users.noreply.github.com>
2025-04-28 18:32:05 +0530

    Update count_repeating_digits.java

--- count_repeating_digits.java
+++ count_repeating_digits.java
@@ -1,1 +1,18 @@
+public class Solution {
+    public static int countRepeatingDigits(long n) {
+        // Write your code here
 
+    int[] hash_arr = new int[10];
+    int count=0;
+ while(n>0){
+     int last_digit =  (int)(n%10);
+     hash_arr[last_digit]++;
+     if(hash_arr[last_digit]==2){
+         count++;
+     }
+     n/=10;
+ }
+ return count;
+}
+    
+}
